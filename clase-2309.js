39a1325012611893a05c87031aa2766dd0f3b2e7
RaulAM <raulartilesm@gmail.com>
2024-09-23 18:46:23 +0100

    Update: procesamiento de clase en el hospital

---
+++ clase-2309.js
@@ -0,0 +1,17 @@
+console.log(window)
+
+const primerParr = document.getElementById('primerParr')
+
+console.log(primerParr)
+
+const mainDiv = document.getElementsByClassName('main-div')
+
+console.log(mainDiv)
+
+const parrafosPrueba = document.getElementsByClassName('parrafosPrueba')
+
+console.log(parrafosPrueba)
+
+const allDivs = document.getElementsByTagName('div')
+
+console.log(allDivs)
